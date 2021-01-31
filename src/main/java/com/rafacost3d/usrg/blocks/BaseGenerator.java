2e81211481935450f37ba9abf166b6b4115de292
Brett Hewitson <bletch1971@hotmail.com>
2021-01-31 12:58:53 +1000

    Block Rotation
    1. changed the generator blocks so they can be rotated.

--- src/main/java/com/rafacost3d/usrg/blocks/BaseGenerator.java
+++ src/main/java/com/rafacost3d/usrg/blocks/BaseGenerator.java
@@ -2,15 +2,19 @@ package com.rafacost3d.usrg.blocks;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.HorizontalBlock;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.material.PushReaction;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.state.StateContainer;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.shapes.IBooleanFunction;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.IBlockReader;
 
-public class BaseGenerator extends Block {
+public abstract class BaseGenerator extends HorizontalBlock {
 
     private static final VoxelShape RENDER_SHAPE = VoxelShapes.combineAndSimplify(
             makeCuboidShape(0.0D,  0.0D, 0.0D, 16.0D,  4.0D, 16.0D),
@@ -28,5 +32,20 @@ public class BaseGenerator extends Block {
     @Override
     public VoxelShape getRenderShape(BlockState state, IBlockReader worldIn, BlockPos pos) {
         return RENDER_SHAPE;
+    }
+
+    @Override
+    protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
+        builder.add(HORIZONTAL_FACING);
+    }
+
+    @Override
+    public BlockState getStateForPlacement(BlockItemUseContext context) {
+        return this.getDefaultState().with(HORIZONTAL_FACING, context.getPlacementHorizontalFacing().getOpposite());
+    }
+
+    @Override
+    public PushReaction getPushReaction(BlockState state) {
+        return PushReaction.BLOCK;
     }
 }
